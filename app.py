3c072ceffc883530559b6e43050f440a46386c00
nishiyu <y.nishikawa.sys22@morijyobi.ac.jp>
2023-10-05 13:32:52 +0900

    10/5 13:32

--- app.py
+++ app.py
@@ -1,79 +1,72 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+# app.py
+
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
-from db import get_connection
-from datetime import datetime
+import os
+from db import insert_blog, insert_image, get_blog_details, get_images
+import base64
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+UPLOAD_FOLDER = 'uploads'  # 画像のアップロード先フォルダ
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
 
-# アップロードされた画像ファイルを保存するディレクトリ
-UPLOAD_FOLDER = 'static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# アップロード先フォルダが存在しない場合、自動的に作成
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
 def allowed_file(filename):
-    # 拡張子のチェックを行う関数
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def index():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM blog')
-    blog_posts = cursor.fetchall()
-    conn.close()
-    return render_template('top.html', posts=blog_posts)
+def top():
+    blogs = get_blog_details()
+    return render_template('top.html', blogs=blogs)
 
 @app.route('/write', methods=['GET', 'POST'])
 def write():
     if request.method == 'POST':
         title = request.form['title']
-        date_str = request.form['date']
-
-        try:
-            # フォームから取得した日付文字列を datetime オブジェクトに変換
-            date = datetime.strptime(date_str, '%Y-%m-%d')
-        except ValueError:
-            return "無効な日付形式です。正しい形式は 'YYYY-MM-DD' です。", 400
-
+        date = request.form['date']
         content = request.form['content']
+        image_files = request.files.getlist('image')
 
-        if 'image' not in request.files:
-            return "画像が選択されていません", 400
+        if not title or not date or not content:
+            flash('タイトル、日付、記事内容は必須です')
+        else:
+            # 記事を挿入
+            blog_id = insert_blog(title, date, content)
 
-        image = request.files['image']
+            if blog_id is not None:  # blog_id が None でないことを確認
+                # 画像をアップロード
+                for file in image_files:
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename)
+                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                        with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'rb') as img_file:
+                            image_data = img_file.read()
+                        insert_image(blog_id, filename, image_data)
 
-        if image.filename == '':
-            return "画像ファイルがありません", 400
-
-        if image and allowed_file(image.filename):
-            image_filename = secure_filename(image.filename)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_filename))
-
-            conn = get_connection()
-            cursor = conn.cursor()
-            cursor.execute('INSERT INTO blog (title, image, date, content) VALUES (%s, %s, %s, %s)',
-                           (title, image.read(), date, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+                flash('記事が投稿されました')
+                return redirect(url_for('top'))
 
     return render_template('write.html')
 
-@app.route('/read/<int:post_id>')
-def read(post_id):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM blog WHERE id = %s', (post_id,))
-    post = cursor.fetchone()
-    conn.close()
 
-    if post is None:
-        return "記事が見つかりません", 404
+@app.route('/read/<int:blog_id>')
+def read(blog_id):
+    blog = get_blog_details(blog_id)
+    images = get_images(blog_id)
+    
+    # 画像データをBase64エンコードして渡す
+    image_data_base64 = []
+    for image in images:
+        image_data_base64.append(base64.b64encode(image[1]).decode('utf-8'))
+    
+    return render_template('read.html', blog=blog, images=images, image_data_base64=image_data_base64)
 
-    # 画像を表示するためにバイナリデータをBase64にエンコード
-    image_data = post[2]  # 画像データは3番目の列にあると仮定
-
-    return render_template('read.html', post=post, image_data=image_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- db.py
+++ db.py
@@ -1,8 +1,89 @@
 # db.py
+
 import os
 import psycopg2
 
+# データベース接続用の関数
 def get_connection():
     url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     return connection
+
+# ブログ記事を挿入する関数
+def insert_blog(title, date, content):
+    sql = "INSERT INTO blog (title, date, content) VALUES (%s, %s, %s) RETURNING id"
+    args = (title, date, content)
+    
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, args)
+        blog_id = cursor.fetchone()[0]
+        connection.commit()
+        return blog_id
+    except psycopg2.DatabaseError:
+        return None
+    finally:
+        cursor.close()
+        connection.close()
+
+# 画像を挿入する関数
+def insert_image(blog_id, filename, image_data):
+    sql = "INSERT INTO images (blog_id, filename, image_data) VALUES (%s, %s, %s)"
+    args = (blog_id, filename, image_data)
+    
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, args)
+        connection.commit()
+    except psycopg2.DatabaseError:
+        pass  # エラーハンドリングは必要に応じて追加してください
+    finally:
+        cursor.close()
+        connection.close()
+
+# すべてのブログ記事を取得する関数
+def get_blog_details(blog_id=None):
+    if blog_id is not None:
+        sql = "SELECT title, date, content FROM blog WHERE id = %s"
+        args = (blog_id,)
+    else:
+        sql = "SELECT id, title, date FROM blog"
+        args = None
+    
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        if args is not None:
+            cursor.execute(sql, args)
+        else:
+            cursor.execute(sql)
+        if blog_id is not None:
+            blog = cursor.fetchone()
+            return blog
+        else:
+            blogs = cursor.fetchall()
+            return blogs
+    except psycopg2.DatabaseError:
+        return [] if blog_id is None else None
+    finally:
+        cursor.close()
+        connection.close()
+
+# 特定のブログ記事の画像を取得する関数
+def get_images(blog_id):
+    sql = "SELECT filename, image_data FROM images WHERE blog_id = %s"
+    args = (blog_id,)
+    
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, args)
+        images = cursor.fetchall()
+        return images
+    except psycopg2.DatabaseError:
+        return []
+    finally:
+        cursor.close()
+        connection.close()
